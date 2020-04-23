7aad82660769a9177da114e2f3a7866f7ae31a9e
Carrey Cheang <carrey.120.cc@gmail.com>
2020-04-23 08:46:28 +0800

    finish about page

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('multiple-items').slick({
+        infinite: true,
+        slidesToSlow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        autoPlay: true,
+        autoPlaySpeed: 2000,
+    });
+});
